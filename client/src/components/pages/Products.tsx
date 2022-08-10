03391a37f301862674fa62210dfae2ab283b819c
jorge machado <jrms_jorgesilva@hotmail.com>
2022-08-10 11:32:13 -0300

    feat: added CRUD operations to products route with axios fetch

--- client/src/components/pages/Products.tsx
+++ client/src/components/pages/Products.tsx
@@ -3,6 +3,9 @@ import ProductsType from "../../@types/ProductsType";
 import { FaTrash, FaEdit, FaPlus, FaSearch } from "react-icons/fa";
 // components
 import Swal from "sweetalert2";
+import axios from "axios";
+
+const FETCH_URL = `http://localhost:8000/products`;
 
 const Products = () => {
   const [productsList, setProductsList] = useState<ProductsType[]>(() => [
@@ -63,6 +66,19 @@ const Products = () => {
     },
   ]);
 
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get(FETCH_URL);
+        setProductsList(response.data);
+      } catch (error: any) {
+        console.error(error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   const addProduct = () => {
     Swal.fire({
       title: "Titulo do novo produto",
@@ -95,11 +111,8 @@ const Products = () => {
                   showCancelButton: true,
                   confirmButtonText: "Continuar",
                   showLoaderOnConfirm: true,
-                  preConfirm: (newPrice) => {
+                  preConfirm: async (newPrice) => {
                     const newProductList = {
-                      _id: Math.floor(
-                        Math.random() * Number.MAX_SAFE_INTEGER
-                      ).toString(),
                       id_product: Math.floor(
                         Math.random() * Number.MAX_SAFE_INTEGER
                       ).toString(),
@@ -116,10 +129,95 @@ const Products = () => {
                       name_categorys: newNameCategory,
                       selected: false,
                     };
+                    await axios.post(FETCH_URL, newProductList);
                     setProductsList((oldProductList) => [
                       ...productsList,
                       newProductList,
                     ]);
+                  },
+                });
+              },
+            });
+          },
+        });
+      },
+    });
+  };
+
+  const deleteProduct = async (id?: string) => {
+    console.log("deleted the id: ", id);
+    const filteredProductsList = productsList.filter((prod) => prod._id !== id);
+    try {
+      await axios.delete(`${FETCH_URL}/${id}`);
+      setProductsList(filteredProductsList);
+    } catch (error: any) {
+      return console.log(error);
+    }
+  };
+
+  const updateProduct = async (id?: string): Promise<void> => {
+    console.log("updated the id: ", id);
+    Swal.fire({
+      title: "Titulo do novo produto",
+      input: "text",
+      cancelButtonText: "Cancelar",
+      showCancelButton: true,
+      confirmButtonText: "Continuar",
+      showLoaderOnConfirm: true,
+      preConfirm: (newNameProduct) => {
+        Swal.fire({
+          title: "Categoria do novo produto",
+          input: "text",
+          cancelButtonText: "Cancelar",
+          showCancelButton: true,
+          confirmButtonText: "Continuar",
+          showLoaderOnConfirm: true,
+          preConfirm: (newNameCategory) => {
+            Swal.fire({
+              title: "Categoria do novo produto",
+              input: "text",
+              cancelButtonText: "Cancelar",
+              showCancelButton: true,
+              confirmButtonText: "Continuar",
+              showLoaderOnConfirm: true,
+              preConfirm: (newCategory) => {
+                Swal.fire({
+                  title: "Preço do novo produto",
+                  input: "number",
+                  cancelButtonText: "Cancelar",
+                  showCancelButton: true,
+                  confirmButtonText: "Continuar",
+                  showLoaderOnConfirm: true,
+                  preConfirm: async (newPrice) => {
+                    const newProductList = {
+                      id_product: Math.floor(
+                        Math.random() * Number.MAX_SAFE_INTEGER
+                      ).toString(),
+                      category: newCategory,
+                      fk_idcategorys: Math.floor(
+                        Math.random() * Number.MAX_SAFE_INTEGER
+                      ).toString(),
+                      name_product: newNameProduct,
+                      price: newPrice,
+                      image: "./images/produtos/placeholder.jpg",
+                      id_categorys: Math.floor(
+                        Math.random() * Number.MAX_SAFE_INTEGER
+                      ).toString(),
+                      name_categorys: newNameCategory,
+                      selected: false,
+                    };
+                    const filteredProductsList = productsList.filter(
+                      (prod) => prod._id !== id
+                    );
+                    try {
+                      await axios.patch(`${FETCH_URL}/${id}`);
+                      setProductsList((oldProductList) => [
+                        ...filteredProductsList,
+                        newProductList,
+                      ]);
+                    } catch (error) {
+                      return console.error(error);
+                    }
                   },
                 });
               },
@@ -141,10 +239,18 @@ const Products = () => {
         />
         <FaSearch className="input__search--icon" />
         <ul className="card">
-          {productsList.map((prod) => (
+          {productsList?.map((prod) => (
             <li className="card__body" key={prod._id}>
-              <FaTrash className="card__btn--delete" role="button" />
-              <FaEdit className="card__btn--edit" role="button" />
+              <FaTrash
+                className="card__btn--delete"
+                role="button"
+                onClick={() => deleteProduct(prod._id)}
+              />
+              <FaEdit
+                className="card__btn--edit"
+                role="button"
+                onClick={() => updateProduct(prod._id)}
+              />
               <img src={prod.image} alt={prod.name_product} />
               <p className="card__text--title">{prod.name_product}</p>
               <span
